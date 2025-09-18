0aa066b004d47a70711fa27353df071f9170cd31
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-09-17 23:57:36 -0500

    first commit

---
+++ src/test/java/com/repobackend/api/InventarioRepuestosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repobackend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioRepuestosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
